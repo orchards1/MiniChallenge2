f1d56956727cd7dd6aef0977e1a7af7a38540897
Michael <jagosikat@yahoo.co.id>
2019-07-11 19:06:04 +0700

    Initial Commit

---
+++ MiniChallenge2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MiniChallenge2 WatchKit Extension
+//
+//  Created by Michael Louis on 11/07/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ MiniChallenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniChallenge2
+//
+//  Created by Michael Louis on 11/07/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
