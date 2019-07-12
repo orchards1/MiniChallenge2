3521d718d218c78b58ab895288cdbb1d2ef4eb18
Marcel Wira <marcelwira20@gmail.com>
2019-07-12 18:56:56 +0700

    ngedit storyboard mountain list

---
+++ MiniChallenge2/MountainsList/MountainsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MountainsListViewController.swift
+//  MiniChallenge2
+//
+//  Created by Marcel W on 12/07/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import UIKit
+
+class MountainsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
