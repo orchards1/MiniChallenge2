5604cec66237e9f79de73b59fdb8102a23f1c82f
Marcel Wira <marcelwira20@gmail.com>
2019-07-12 20:35:39 +0700

    friday, 12 juli 2019 8.35 pm

--- MiniChallenge2/MountainsList/MountainsListViewController.swift
+++ MiniChallenge2/MountainsList/MountainsListViewController.swift
@@ -8,26 +8,48 @@
 
 import UIKit
 
-class MountainsListViewController: UIViewController {
+class MountainsListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var mountainCollectionView: UICollectionView!
+    
+    var arrayImage = [UIImage(named: "ic_onboarding_1"), UIImage(named: "ic_onboarding_2"), UIImage(named: "ic_onboarding_3"), UIImage(named: "ic_onboarding_4"), UIImage(named: "ic_onboarding_5")]
+    var arrayMountainName = ["Mt. Semeru", "Mt. Gede", "Mt. Pangrango", "Mt. Suparman", "Mt. Sukirman"]
+    var arrayLocation = ["East Java, Java Island", "Location, Island", "Location, Island", "Location, Island", "Location, Island"]
+    var arrayAltitude = ["3676 mdpl", "0012 mdpl", "2312 mdpl", "1012 mdpl", "1921 mdpl"]
+    
+    var mountain = [Mountain]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mountainCollectionView.delegate = self
+        mountainCollectionView.dataSource = self
+        for i in 0..<arrayMountainName.count{
+            let data = Mountain(mountainImg: arrayImage[i]!, name: arrayMountainName[i], location: arrayLocation[i], altitude: arrayAltitude[i])
+            mountain.append(data)
+        }
+        setupNavBar()
         // Do any additional setup after loading the view.
     }
     
     func setupNavBar(){
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mountain.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MountainListCollectionViewCell
         
+        cell?.imageMountain.image = mountain[indexPath.row].mountainImg
+        cell?.nameLabel.text = mountain[indexPath.row].name
+        cell?.locationLabel.text = mountain[indexPath.row].location
+        cell?.heightLabel.text = mountain[indexPath.row].altitude
+        
+        return cell!
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
