44e9199939dd91e77c4bf31ef8cf7447be5b569f
Michael <jagosikat@yahoo.co.id>
2019-07-12 14:37:11 +0700

    test beberapa

---
+++ MiniChallenge2/OnBoardingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OnBoardingViewController.swift
+//  MiniChallenge2
+//
+//  Created by Michael Louis on 12/07/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MiniChallenge2/OnboardingSlide.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingSlide.swift
+//  MiniChallenge2
+//
+//  Created by Michael Louis on 12/07/19.
+//  Copyright © 2019 Michael Louis. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingSlide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
